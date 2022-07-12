174b59584097cc1ed075df9ba03d2f522505e0f6
Juan Savedra <juansavedra827@gmail.com>
2022-07-12 13:35:38 -0300

    show password

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,20 @@
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
 const emailInput = document.querySelector(".emailInput");
 const passwordInput = document.querySelector(".passwordInput");
+
 const user = {
   email: "",
   password: "",
 };
 
 let isDarkTheme = true;
+
+function showPassword() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
 
 function saveUserData() {
   user.email = emailInput.value;
@@ -22,6 +29,9 @@ function saveUserData() {
 }
 
 function toggleTheme() {
+  const body = document.querySelector("body");
+  const container = document.querySelector(".container");
+
   if (!isDarkTheme) {
     body.style.backgroundColor = "#191816";
     container.style.backgroundColor = "#24221f";
