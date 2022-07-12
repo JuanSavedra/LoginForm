5aa28b9ed70d5e9f56edab47f582efd311a0bc5c
Juan Savedra <juansavedra827@gmail.com>
2022-07-12 11:30:07 -0300

    saving user data

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const emailInput = document.querySelector(".emailInput");
+const passwordInput = document.querySelector(".passwordInput");
 
 let isDarkTheme = true;
+
+const user = {
+  email: "",
+  password: "",
+};
+
+function saveUserData() {
+  user.email = emailInput.value;
+  user.password = passwordInput.value;
+
+  const userData = {
+    email: user.email,
+    password: user.password,
+  };
+
+  console.log(userData);
+}
 
 function toggleTheme() {
   if (!isDarkTheme) {
